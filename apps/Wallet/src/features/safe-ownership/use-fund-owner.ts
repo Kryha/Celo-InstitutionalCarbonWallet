08a177d07d542fcb6a50b3f836824508cdae1b3b
ivan <ivan@kryha.io>
2023-10-11 15:18:14 +0200

    adding untracked files

---
+++ apps/Wallet/src/features/safe-ownership/use-fund-owner.ts
@@ -0,0 +1,42 @@
+import { EthereumRpc } from "@/features";
+import { useWalletStore } from "@/store";
+import { useMutation } from "@tanstack/react-query";
+import { GOERLI_FUND_GAS_AMOUNT } from "./constants";
+
+export function useFundSafeOwner() {
+  const setBalance = useWalletStore((state) => state.setBalance);
+  const web3Auth = useWalletStore((state) => state.web3Auth);
+  const pk = process.env.NEXT_PUBLIC_OWNER_1_PRIVATE_KEY_GOERLI!;
+  return useMutation({
+    mutationFn: (address: string) => {
+      console.log("addr", address);
+      return fetch("/api/safe/transaction", {
+        method: "POST",
+        body: JSON.stringify({
+          pk: pk,
+          amount: GOERLI_FUND_GAS_AMOUNT,
+          destination: address
+        }),
+      });
+    },
+    onSuccess: async () => {
+      if (web3Auth && web3Auth.provider) {
+        const rpc = new EthereumRpc(web3Auth.provider);
+        const balance = await rpc.getBalance();
+        setBalance(balance);
+      }
+    },
+  });
+}
+
+async function setNewBalance() {
+  const setBalance = useWalletStore((state) => state.setBalance);
+  const web3Auth = useWalletStore((state) => state.web3Auth);
+  
+  if (web3Auth && web3Auth.provider) {
+    
+    const rpc = new EthereumRpc(web3Auth.provider);
+    const balance = await rpc.getBalance();
+    setBalance(balance);
+  }
+}

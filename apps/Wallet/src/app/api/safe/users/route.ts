1c5ef266365a97dc4cc15ebaebc1f80f45a0aa5c
ivan <ivan@kryha.io>
2023-10-20 13:49:52 +0200

    added getters

---
+++ apps/Wallet/src/app/api/safe/users/route.ts
@@ -0,0 +1,14 @@
+import { ethers } from "ethers";
+import { rbacModuleAddress, start, pageSize } from "../util/constants";
+import { getEtherscanSigner } from "../util/utils";
+import { Rbac } from "../util/typechain/types/config/abis";
+import rbac from "../util/typechain/abis/rbac.json";
+
+export async function GET(req: Request, res: Response): Promise<Response> {
+    const signer = getEtherscanSigner(process.env.OWNER_1_PRIVATE_KEY_GOERLI!);
+    const moduleContract = new ethers.Contract(rbacModuleAddress, rbac, signer) as Rbac;
+    const safe = process.env.SAFE_ADDRESS!;
+    const users = await moduleContract.getDelegates(safe, start, pageSize);
+    
+    return new Response(JSON.stringify(users));
+  }
--- apps/Wallet/src/app/api/safe/util/constants.ts
+++ apps/Wallet/src/app/api/safe/util/constants.ts
@@ -2,5 +2,6 @@ export const rbacModuleAddress = "0xfc3c71d00d522f8a2537523b22f54405011257ff";
 // etherscan signer constants
 export const network = "goerli";
 export const etherscanId = "9QA7YFDY8APXCMB32IU6KUEA6F83HH6VPZ";
-
-
+// get delegates constants
+export const start = 0;
+export const pageSize = 100;

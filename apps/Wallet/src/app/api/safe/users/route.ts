893af0fad7ba459281b5a11eedae7b349a752ef0
ivan <ivan@kryha.io>
2023-10-24 17:12:59 +0200

    users route

--- apps/Wallet/src/app/api/safe/users/route.ts
+++ apps/Wallet/src/app/api/safe/users/route.ts
@@ -1,11 +1,11 @@
+import { ethers } from "ethers";
 import { Rbac__factory } from "../../../../types/typechain/types/config/abis";
 import { pageSize, start } from "../util/constants";
-import { getEtherscanSigner } from "../util/utils";
 
 export async function GET(req: Request, res: Response): Promise<Response> {
-    const signer = getEtherscanSigner(process.env.OWNER_1_PRIVATE_KEY_GOERLI!);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!);
     const rbacModuleAddress = process.env.RBAC_MODULE_ADDRESS!;
-    const rbac = Rbac__factory.connect(rbacModuleAddress, signer);
+    const rbac = Rbac__factory.connect(rbacModuleAddress, provider);
     const safe = process.env.SAFE_ADDRESS!;
     const users = await rbac.getDelegates(safe, start, pageSize);
 

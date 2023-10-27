cf19204a4b7ff2ce458fd65b0a54bfe8d93f6d86
Gabriel Kidane <gabrielkidane@Gabriels-MBP.localdomain>
2023-10-27 11:36:26 +0200

    cleanup logs

--- apps/Wallet/src/app/api/safe/history/transactions/route.ts
+++ apps/Wallet/src/app/api/safe/history/transactions/route.ts
@@ -7,7 +7,5 @@ export async function GET(req: Request, res: Response): Promise<Response> {
 
   const history = txs.results;
 
-    console.log("check", history[0]);
-
   return new Response(JSON.stringify(history));
 }

6c3965e24d858fd7a100aebf0615f536e3f8c666
tim kryha <tim@kryha.io>
2023-10-27 10:23:46 +0200

    update type name

--- apps/Wallet/src/models/User.ts
+++ apps/Wallet/src/models/User.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export interface Users extends mongoose.Document {
+export interface User extends mongoose.Document {
   name: string;
   surname: string;
   publicKey: string;
@@ -8,7 +8,7 @@ export interface Users extends mongoose.Document {
   role: string;
 }
 
-const UserSchema = new mongoose.Schema<Users>({
+const UserSchema = new mongoose.Schema<User>({
   name: {
     type: String,
     required: [true, "Please provide a name for the user."],
@@ -33,4 +33,4 @@ const UserSchema = new mongoose.Schema<Users>({
   },
 });
 
-export default mongoose.models.User || mongoose.model<Users>("User", UserSchema);
+export default mongoose.models.User || mongoose.model<User>("User", UserSchema);

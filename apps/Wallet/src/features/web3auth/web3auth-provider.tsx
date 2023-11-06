72e7c8415f66ec7212144da07f9641e1706152e6
tim kryha <tim@kryha.io>
2023-11-06 13:39:55 +0100

    persist login when logged in user refreshes

--- apps/Wallet/src/features/web3auth/web3auth-provider.tsx
+++ apps/Wallet/src/features/web3auth/web3auth-provider.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { WEB3AUTH_CLIENT_ID } from "@/constants";
-import { ADAPTER_SETTINGS, GOERLI_CHAIN_CONFIG } from "@/features";
+import { ADAPTER_SETTINGS, GOERLI_CHAIN_CONFIG, useUpdateWalletStore } from "@/features";
 import { useWalletStore } from "@/store";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { Web3AuthNoModal } from "@web3auth/no-modal";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export function Web3AuthProvider({ children }: { children: React.ReactNode }) {
   const web3Auth = useWalletStore((state) => state.web3Auth);
   const setWeb3Auth = useWalletStore((state) => state.setWeb3Auth);
   const clientId = WEB3AUTH_CLIENT_ID;
+  const updateWalletStore = useUpdateWalletStore();
+  const { push } = useRouter();
 
   useEffect(() => {
     const init = async () => {
@@ -33,6 +36,11 @@ export function Web3AuthProvider({ children }: { children: React.ReactNode }) {
         await web3AuthInstance.init();
 
         setWeb3Auth(web3AuthInstance);
+
+        if (web3AuthInstance.connected) {
+          await updateWalletStore(web3AuthInstance);
+          push("/dashboard");
+        }
       } catch (error) {
         console.error(error);
       }
